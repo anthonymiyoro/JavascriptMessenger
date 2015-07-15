61bc224de241e518464391028d484fb63a4fc5ca
anthonymiyoro <amiyoro@gmail.com>
2015-07-15 18:16:11 +0300

    added send function

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,19 @@
 // YOUR CODE HERE:
 
+// sends a message
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://api.parse.com/1/classes/chatterbox',
+    // Shows type of message
+  type: 'POST',
+    // Changes the message to a string
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+});
