64651a9ee34b981eba3e3abeb0cc225b993689e2
anthonymiyoro <amiyoro@gmail.com>
2015-07-17 15:46:52 +0300

    completed send function

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,13 +1,14 @@
 // YOUR CODE HERE:
 
 // sends a message
+function send(){
 $.ajax({
   // This is the url you should use to communicate with the parse API server.
   url: 'https://api.parse.com/1/classes/chatterbox',
-    // Shows type of message
+    // Shows type of function
   type: 'POST',
     // Changes the message to a string
-  data: JSON.stringify(message),
+  data: JSON.stringify(message.value),
   contentType: 'application/json',
   success: function (data) {
     console.log('chatterbox: Message sent');
@@ -17,3 +18,23 @@ $.ajax({
     console.error('chatterbox: Failed to send message');
   }
 });
+}
+
+// retrieves content from server
+// gets a message
+function recieve(){
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://api.parse.com/1/classes/chatterbox',
+    // Shows type of function
+  type: 'GET',
+  contentType: 'application/json',
+  success: function (data) {
+  console.log('chatterbox: Message RECIEVED');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to recieve message');
+  }
+});
+}
