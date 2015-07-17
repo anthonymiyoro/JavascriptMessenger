53453603677068bfb15249c49fe9e5eb9af41c23
anthonymiyoro <amiyoro@gmail.com>
2015-07-17 22:01:27 +0300

    added a recieve function

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -22,19 +22,18 @@ $.ajax({
 
 // retrieves content from server
 // gets a message
-function recieve(){
-$.ajax({
+function get(){
+ $.ajax({
   // This is the url you should use to communicate with the parse API server.
   url: 'https://api.parse.com/1/classes/chatterbox',
-    // Shows type of function
   type: 'GET',
+  data: JSON.stringify(message.value),
   contentType: 'application/json',
   success: function (data) {
-  console.log('chatterbox: Message RECIEVED');
-  },
-  error: function (data) {
-    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    console.error('chatterbox: Failed to recieve message');
+   $.each(data.results, function(i,message){
+      
+      $("#recieved").append("<li>" +message.username +"<br>"+ message.text + "</li>");
+   });
   }
-});
+ });
 }
